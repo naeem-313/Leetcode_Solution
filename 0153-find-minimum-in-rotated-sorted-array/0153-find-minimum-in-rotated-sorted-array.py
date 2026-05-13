2c3420151307bff3cfa4c9ddbf0478c42fb3cbaa
Naeem Abbas <nayeemabbas313@gmail.com>
2026-05-14 00:18:55 +0800

    Time: 0 ms (100%), Space: 12.5 MB (68.65%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findMin(self, nums):
+
+        return min(nums)
+        
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
