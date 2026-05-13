f44cb4f9966bd28798e351f1976dcb99f8f35a07
Naeem Abbas <nayeemabbas313@gmail.com>
2026-05-13 23:03:18 +0800

    Time: 3 ms (32.97%), Space: 13.1 MB (43.85%) - LeetHub - Time: O(n), Space: O(n)

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -4,20 +4,20 @@ class Solution(object):
         num1=list(num)
         n=len(num1)
 
+        
+
         if n<3:
             return max(num1)
 
         num1.remove(max(num1))
         num1.remove(max(num1))
 
-
         return max(num1)
+
+
        
-
-       
-
-
-   
         
-         
-        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
